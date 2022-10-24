ffd0c54bb9b342c7efe80470fa99039e208de1e4
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-10-24 21:44:37 +0300

    I am setup router installed and add create Browser router

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+root.render( <React.StrictMode >
+    <Provider store = { store } >
+    <BrowserRouter><App /></BrowserRouter>
+
+    </Provider> </React.StrictMode>
 );
