58ce92f5a36a85f67bfee38a80d02adf06e488ce
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-10-25 20:49:32 +0300

    adding get component

---
+++ src/componets/GetInTouchComponet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function GetInTouchComponet() {
+  return (
+      <div className='bg-white '>
+      
+      <h1 className='text-[34px] font-medium leading-8 '>Get in touch</h1>
+      </div>
+  )
+}
+
+export default GetInTouchComponet
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import { BrowserRouter } from "react-router-dom";
+
 import App from './App';
 import './index.css';
 
@@ -11,7 +12,9 @@ const root = createRoot(container);
 
 root.render( <React.StrictMode >
     <Provider store = { store } >
-    <BrowserRouter><App /></BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 
     </Provider> </React.StrictMode>
 );
