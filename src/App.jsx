33046665d4914ce39aaaec1cca6863d4a26f2953
Iman <96542573+Imanzuher@users.noreply.github.com>
2022-10-26 21:48:54 +0300

    Completed Hero section (#67)
    * Completed Hero section
    
    * Done with snapshot testing
    
    * added a missing dependency for snapshot testing
    
    Co-authored-by: allan <allansaleh95@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 // import { Routes, Route } from "react-router-dom";
-
+import Hero from './components/Hero/Hero';
 import './App.css';
 
 function App() {
-    return ( <div className = "App" >
-        <h1>Hello</h1>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Hero />
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
