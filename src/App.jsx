3420da38f5f490e7dea274d87de27237a7c897a0
Imanzuher <imoojaf@gmail.com>
2022-10-26 22:34:55 +0300

    Done with why choose us component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 // import { Routes, Route } from "react-router-dom";
 import Hero from './components/Hero/Hero';
+import WyChooseUs from './components/whyChooseUs/WhyChooseUs';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <WyChooseUs />
       <h1>Hello</h1>
     </div>
   );
