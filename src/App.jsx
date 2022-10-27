6d78f3f66687eb33c58a30428103d18643d413cc
Paywand Mohammed Hussein <73426000+paywand007@users.noreply.github.com>
2022-10-27 16:31:40 +0300

    Finish getin touch form componet

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import { Routes, Route } from "react-router-dom";
 
 import './App.css';
+
 import GetInTouchForm from './componets/GetInTouchForm';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     return (
        
         <div className="App">
-        <GetInTouchForm />
+ <GetInTouchForm/>
         </div>
         
     );
