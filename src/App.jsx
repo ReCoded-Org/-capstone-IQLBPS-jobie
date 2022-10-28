2c0b347a21799ba3c6f62a35f7da649cb2568003
Lawen <lawensardar16@gmail.com>
2022-10-28 21:30:07 +0300

    completed about us component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import AboutUsComponent from './Components/AboutUsComponent';
 // import { Routes, Route } from "react-router-dom";
 
 import './App.css';
 
 function App() {
     return ( <div className = "App" >
-        <h1>Hello</h1>
+       
+        <AboutUsComponent />
         </div>
     );
 }
