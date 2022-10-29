ae5f5a038b41a5747ebc6ffff4508cb5f92fc5ab
zakariaahmad <kimo.ahmad2009@gmail.com>
2022-10-29 19:54:52 +0300

    Done and its responsive for small medium and large

--- src/components/About/HowItWorks/HowItWorks.jsx
+++ src/components/About/HowItWorks/HowItWorks.jsx
@@ -3,25 +3,31 @@ import {faFileLines,faHandshake,faPen} from '@fortawesome/free-solid-svg-icons'
 
 function HowItWorks(){
   return(
-    <div className="md:container md:mx-auto pl-9 ">
+    <div className="container  pl-9 ">
       <div>
-      <h1 className="text-left text-5xl font-inter text-secondary py-10">How it Works</h1>
+      <h1 className="text-left text-5xl font-inter text-secondary py-10 font-bold">How it Works</h1>
       </div>
-      <div className="text-left flex justify-between flex-nowrap content-center text- font-inter ">
-        <div className='w-1/2'>
-          <FontAwesomeIcon icon={faFileLines} className="rounded-full bg-indigo-500 text-4xl"/>
-          <h1 className='text-3xl'>Create ur resume </h1>
-          <p>Create your resume and add your skills and experience to your profile.You can also add your education and work experience to your profile.</p>
+      <div className="text-left flex justify-center flex-nowrap content-center text- font-inter gap-24 small:flex-col small:items-center medium:flex-col pb-10">
+        <div className=' '>
+          <div className='rounded-full bg-dark-gray h-16 w-16 flex items-center justify-center '>
+              <FontAwesomeIcon icon={faFileLines} className="text-3xl text-accent"/>
+          </div>
+          <h1 className='text-3xl py-3 small:text-left'>Create your resume </h1>
+          <p className='text-justify small:text-justify small:text-xl'>Create your resume and add your skills and experience to your profile.You can also add your education and work experience to your profile.</p>
         </div>
-        <div className='w-1/2'>
-        <FontAwesomeIcon icon={faPen} className="rounded-full bg-indigo-500 text-4xl"/>
-          <h1 className='text-3xl'>Apply for jobs</h1>
-          <p className=''>Search for jobs and apply for them.You can also search for companires and apply for their jobs.</p>
+        <div className=' '>
+          <div className='rounded-full bg-dark-gray h-16 w-16 flex items-center justify-center'>
+              <FontAwesomeIcon icon={faPen} className="text-3xl text-accent"/>
+          </div>
+          <h1 className='text-3xl py-3 small:text-left'>Apply for jobs</h1>
+          <p className='text-justify small:text-justify small:text-xl'>Search for jobs and apply for them.You can also search for companires and apply for their jobs.</p>
         </div>
-        <div className='w-1/2'>
-          <FontAwesomeIcon icon={faHandshake} className="rounded-full bg-indigo-500 text-4xl"/>
-          <h1 className='text-3xl'>Get Hired</h1>
-          <p>Once you have applied for a job you will be notified when the employer has responded to your application.</p>
+        <div className=' '>
+          <div className='rounded-full bg-secondary h-16 w-16 flex items-center justify-center'>
+            <FontAwesomeIcon icon={faHandshake} className="text-3xl text-accent"/>
+          </div>
+          <h1 className='text-3xl py-3'>Get Hired</h1>
+          <p className='text-justify small:text-justify small:text-xl'>Once you have applied for a job you will be notified when the employer has responded to your application.</p>
         </div>
       </div>
 
