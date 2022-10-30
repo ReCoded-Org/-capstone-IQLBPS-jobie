96ac6869f4be6146072c1893e9327001a46e43a1
Zaynab Azad <zaynabbbazad20@gmail.com>
2022-10-31 01:57:53 +0300

    Jobs Showcase

---
+++ src/components/JobsShowcase/JobsShowcase.jsx
@@ -0,0 +1,18 @@
+import Showcase from './Showcase';
+
+const JobsShowcase = ({ showcaseArray }) => {
+  return (
+    <div className="my-20 flex justify-center gap-x-20 flex-wrap w-screen ">
+      {showcaseArray.map((data) => {
+        return (
+          <Showcase
+            header={data.header}
+            body={data.body}
+            button={data.button}
+          />
+        );
+      })}
+    </div>
+  );
+};
+export default JobsShowcase;
