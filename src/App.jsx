13232e6d7de816302585a873405c8bf025152c08
Zakaria Ahmad <kimo.ahmad2009@gmail.com>
2022-10-30 21:09:54 +0200

    Done and added a hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 // import { Routes, Route } from "react-router-dom";
 import Hero from './components/Hero/Hero';
 import './App.css';
+import SaveButton from './components/SaveButton';
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <h1>Hello</h1>
+      <SaveButton/>
     </div>
   );
 }
