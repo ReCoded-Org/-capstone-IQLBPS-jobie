98b7b155db3ac40bef3500957bedce15e7cc2940
ayaali333 <1aya.dn.ali@gmail.com>
2022-10-30 21:29:08 +0300

    Name And Links Edit Component

---
+++ src/components/NameAndLinks/--Test--/NameAndLinks.test.js
@@ -0,0 +1,10 @@
+import renderer from 'react-test-renderer';
+import NameAndLinks from "../NameAndLinks";
+import React from "react";
+
+it('renders  NameAndLinks component correctly', () => {
+  const tree = renderer
+    .create(<NameAndLinks />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- src/components/NameAndLinks/NameAndLinks.js
+++ src/components/NameAndLinks/NameAndLinks.js
@@ -3,19 +3,24 @@ import React from 'react';
 const NameAndLinks = () => {
   return (
     <div className="">
-      <div className=" flex flex-col   pt-6 pl-44  ">
-        <button
+      <div className="  flex flex-row pt-6  pl-44 ">
+        <div className="bg-black  pt-6 p-10  " >
+            <img  src='https://i.pinimg.com/236x/44/ab/21/44ab21be0821eed03d0943dd5662def8.jpg' className='w-20 bg-blue-100' alt=''/>
+        </div>
+      <div className=" flex flex-col   pt-6  ">
+      <button
           type="button"
-          className="bg-dark-gray hover:bg-[#226086]   mb-4 w-40 text-white font-bold py-2 px-4 rounded-full  "
+          className="bg-dark-gray hover:bg-[#226086]  ml-8  mb-4 w-40 text-white font-bold py-2 px-4 rounded-full  "
         >
           UPload Photo
         </button>
         <button
           type="button"
-          className="bg-transparent hover:bg-dark-gray w-40  mb-4  text-[#154360] font-semibold hover:text-white py-2 px-4 border border-dark-gray hover:border-transparent rounded-full"
+          className="bg-transparent hover:bg-dark-gray w-40  ml-8 mb-4  text-[#154360] font-semibold hover:text-white py-2 px-4 border border-dark-gray hover:border-transparent rounded-full"
         >
           Delete
         </button>
+      </div>
       </div>
       <div className="pt-6 pl-44">
         <form>
