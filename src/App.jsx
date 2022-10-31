1c30a303f17f17c8c0fceabad6b6e50669489812
ayaali333 <90651890+ayaali333@users.noreply.github.com>
2022-10-31 07:41:02 +0300

    Work and education edit component (#72)
    * work and education
    
    * finished work And Education Edit Component
    
    * update snapShot test
    
    Co-authored-by: Allan <allanahmedd@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+
+
+
 // import { Routes, Route } from "react-router-dom";
+
 import Hero from './components/Hero/Hero';
 import WyChooseUs from './components/whyChooseUs/WhyChooseUs';
 import './App.css';
+import Education from './components/EducationAndExperience/Education';
+import Experience from './components/EducationAndExperience/Experience';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <WyChooseUs />
-      <h1>Hello</h1>
+      <Experience/>
+      <Education/>
     </div>
   );
 }
