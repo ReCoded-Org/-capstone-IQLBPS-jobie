ecba8cb6d2ab9b11d32e83cfe686025827bf906f
marwa <mds08@mail.aub.edu>
2022-11-01 22:35:24 +0200

    updated snapshot test

--- src/components/companyShowcaseComponent/_test_/CompanyShowcaseComponent.test.jsx
+++ src/components/companyShowcaseComponent/_test_/CompanyShowcaseComponent.test.jsx
@@ -1,9 +1,10 @@
 import renderer from 'react-test-renderer';
 import CompanyShowcaseComponent from "../CompanyShowcaseComponent.jsx"
+import {showCaseData} from "../../../data/showCaseData"
 
 it('renders Company Showcase Component correctly', () => {
     const tree = renderer
-      .create(<CompanyShowcaseComponent/>)
+      .create(<CompanyShowcaseComponent showCaseData={showCaseData}/>)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
