3a9b2bcecd7bd98e5cddab7fed3c863a6a91329f
shadybot97 <108574192+ShadyBot97@users.noreply.github.com>
2022-11-02 01:49:21 +0300

    Contact_skills

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 // import { Routes, Route } from "react-router-dom";
 import Hero from './components/Hero/Hero';
 import './App.css';
+import ContactSkills from './components/ProfileEdit/contact_skills_page';
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <h1>Hello</h1>
+<ContactSkills/>
     </div>
   );
 }
