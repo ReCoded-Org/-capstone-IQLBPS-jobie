38aaae890e60d96ac82192f932058a52ed9d97cb
zakaria ahmad <108405793+zacks006600@users.noreply.github.com>
2022-11-02 18:49:00 +0200

    Changes Requested are Done and its passing the snapshot testing

--- src/components/About/HowItWorks/HowItWorks.jsx
+++ src/components/About/HowItWorks/HowItWorks.jsx
@@ -3,24 +3,24 @@ import {faFileLines,faHandshake,faPen} from '@fortawesome/free-solid-svg-icons'
 
 function HowItWorks(){
   return(
-    <div className="container  pl-9 ">
+    <div className="container px-20 ">
       <div>
       <h1 className="text-left text-5xl font-inter text-secondary py-10 font-bold">How it Works</h1>
       </div>
-      <div className="text-left flex justify-center flex-nowrap content-center text- font-inter gap-24 small:flex-col small:items-center medium:flex-col pb-10">
+      <div className="text-left flex justify-center flex-nowrap content-center text- font-inter gap-20 small:flex-col small:items-center medium:flex-col pb-9">
         <div className=' '>
           <div className='rounded-full bg-dark-gray h-16 w-16 flex items-center justify-center '>
               <FontAwesomeIcon icon={faFileLines} className="text-3xl text-accent"/>
           </div>
           <h1 className='text-3xl py-3 small:text-left'>Create your resume </h1>
-          <p className='text-justify small:text-justify small:text-xl'>Create your resume and add your skills and experience to your profile.You can also add your education and work experience to your profile.</p>
+          <p className='text-justify small:text-justify small:text-xl'>Create your resume and add your skills and experience to your profile. You can also add your education and work experience to your profile.</p>
         </div>
         <div className=' '>
           <div className='rounded-full bg-dark-gray h-16 w-16 flex items-center justify-center'>
               <FontAwesomeIcon icon={faPen} className="text-3xl text-accent"/>
           </div>
           <h1 className='text-3xl py-3 small:text-left'>Apply for jobs</h1>
-          <p className='text-justify small:text-justify small:text-xl'>Search for jobs and apply for them.You can also search for companires and apply for their jobs.</p>
+          <p className='text-justify small:text-justify small:text-xl'>Search for jobs and apply for them. You can also search for companires and apply for their jobs.</p>
         </div>
         <div className=' '>
           <div className='rounded-full bg-secondary h-16 w-16 flex items-center justify-center'>
