507543c1b4a579c5f4cfdd08dd151260db341218
parykhan-jameel <parexan.00443431@gmail.com>
2022-11-06 22:09:29 +0300

    testing the contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 // import { Routes, Route } from "react-router-dom";
 import Hero from './components/Hero/Hero';
 import './App.css';
+ import Contact  from './components/contact/Contact';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <h1>Hello</h1>
+      <Contact/>
     </div>
   );
 }
