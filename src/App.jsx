1a6b1db726e23d8c7997429463f7c5b90362052a
allan <allansaleh95@gmail.com>
2022-11-06 17:52:10 +0300

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,87 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import BasicInfoEdit from './components/BasicInfoEdit';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import BasicInfoEdit from "./components/BasicInfoEdit/BasicInfoEdit";
 import Navbar from "./components/Navbar/Navbar";
-import AboutUsComponent from './components/AboutUsComponent/AboutUsComponent';
-import Hero from './components/Hero/Hero';
-import Localize from './Localize';
-import MeetOurTeam from './components/MeetOurTeam/MeetOurTeam';
-import { teamMembers } from './data/teamData';
-import WyChooseUs from './components/whyChooseUs/WhyChooseUs';
-import Login from './components/Login/Login';
-import SignUp from './components/signup/SignUp';
-import CompanyShowcaseComponent from './components/companyShowcaseComponent/CompanyShowcaseComponent';
-import { showCaseData } from './data/showCaseData';
-import './App.css';
-import SaveButton from './components/UI/Button/SaveButton';
-import Categories from './components/Categories/Categories';
-import Education from './components/EducationAndExperience/Education';
-import Experience from './components/EducationAndExperience/Experience';
-import GetInTouchForm from './components/gitInTouchForm/GetInTouchForm';
-import JobsShowcase from './components/JobsShowcase/JobsShowcase';
-import { showcaseData } from './data';
-import LatestJobs from './components/LatestJob/LatestJobs';
-import HowItWorks from './components/About/HowItWorks/HowItWorks';
+import AboutUsComponent from "./components/AboutUsComponent/AboutUsComponent";
+import Hero from "./components/Hero/Hero";
+import Localize from "./Localize";
+import MeetOurTeam from "./components/MeetOurTeam/MeetOurTeam";
+import { teamMembers } from "./data/teamData";
+import WyChooseUs from "./components/whyChooseUs/WhyChooseUs";
+import Login from "./components/Login/Login";
+import SignUp from "./components/signup/SignUp";
+import CompanyShowcaseComponent from "./components/companyShowcaseComponent/CompanyShowcaseComponent";
+import { showCaseData } from "./data/showCaseData";
+import "./App.css";
+import SaveButton from "./components/UI/Button/SaveButton";
+import Categories from "./components/Categories/Categories";
+import Education from "./components/EducationAndExperience/Education";
+import Experience from "./components/EducationAndExperience/Experience";
+import GetInTouchForm from "./components/gitInTouchForm/GetInTouchForm";
+import JobsShowcase from "./components/JobsShowcase/JobsShowcase";
+import { showcaseData } from "./data";
+import LatestJobs from "./components/LatestJob/LatestJobs";
+import HowItWorks from "./components/About/HowItWorks/HowItWorks";
 
 const data = [
   {
-    position: 'Web Developer f',
-    salary: '2000$-3000$',
-    date: '10/10/2022',
+    position: "Web Developer f",
+    salary: "2000$-3000$",
+    date: "10/10/2022",
     fullTime: true,
     partTime: false,
     remote: false,
     id: 1,
   },
   {
-    position: 'Web Developer p',
-    salary: '2000$-3000$',
-    date: '10/10/2022',
+    position: "Web Developer p",
+    salary: "2000$-3000$",
+    date: "10/10/2022",
     fullTime: false,
     partTime: true,
     remote: false,
     id: 2,
   },
   {
-    position: 'Web Developer fr',
-    salary: '2000$-3000$',
-    date: '10/10/2022',
+    position: "Web Developer fr",
+    salary: "2000$-3000$",
+    date: "10/10/2022",
     fullTime: true,
     partTime: false,
     remote: true,
     id: 3,
   },
   {
-    position: 'Web Developer pr',
-    salary: '2000$-3000$',
-    date: '10/10/2022',
+    position: "Web Developer pr",
+    salary: "2000$-3000$",
+    date: "10/10/2022",
     fullTime: false,
     partTime: true,
     remote: true,
     id: 4,
   },
   {
-    position: 'Web Developer fr',
-    salary: '2000$-3000$',
-    date: '10/10/2022',
+    position: "Web Developer fr",
+    salary: "2000$-3000$",
+    date: "10/10/2022",
     fullTime: true,
     partTime: false,
     remote: true,
     id: 5,
   },
   {
-    position: 'Web Developer r',
-    salary: '2000$-3000$',
-    date: '10/10/2022',
+    position: "Web Developer r",
+    salary: "2000$-3000$",
+    date: "10/10/2022",
     fullTime: false,
     partTime: false,
     remote: true,
     id: 6,
   },
   {
-    position: 'Web Developer fp',
-    salary: '2000$-3000$',
-    date: '10/10/2022',
+    position: "Web Developer fp",
+    salary: "2000$-3000$",
+    date: "10/10/2022",
     fullTime: true,
     partTime: true,
     remote: false,
@@ -89,10 +89,10 @@ const data = [
   },
 ];
 const header = [
-  { title: 'Position', id: 1 },
-  { title: 'Salary', id: 2 },
-  { title: 'Date', id: 3 },
-  { title: '', id: 4 },
+  { title: "Position", id: 1 },
+  { title: "Salary", id: 2 },
+  { title: "Date", id: 3 },
+  { title: "", id: 4 },
 ];
 function App() {
   const { t } = useTranslation();
