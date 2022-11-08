81f28996d0b815821b9645f679edf4ae049b009e
parykhan-jameel <parexan.00443431@gmail.com>
2022-11-08 20:10:13 +0300

    Contact and skills done

---
+++ src/components/contact/__Test__/Contact.test.js
@@ -0,0 +1,12 @@
+import renderer from 'react-test-renderer';
+import React from "react";
+import Contact from '../Contact';
+
+
+
+it('renders Contact', () => {
+  const tree = renderer
+    .create(<Contact/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
