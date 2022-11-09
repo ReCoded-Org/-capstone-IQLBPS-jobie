6e30562bde5bd131bdea15e00977c415f0120af7
Marwa Salameh <108797405+marwazpov@users.noreply.github.com>
2022-11-09 16:02:39 +0200

    completed working on the functionality for the sorting button found in the filter results component  (#110)
    * worked on functionality of the sorting button
    
    * ran yarn fix
    
    Co-authored-by: allan <allansaleh95@gmail.com>

--- src/components/FilterResults/FilterButton.jsx
+++ src/components/FilterResults/FilterButton.jsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
 
-function FilterButton() {
+function FilterButton({ handleChange }) {
   return (
     <Menu
       as="div"
@@ -34,6 +34,7 @@ function FilterButton() {
                 <div
                   data-testid="language-button-option"
                   aria-hidden="true"
+                  onClick={(e) => handleChange("newest", e)}
                   className={`${
                     active
                       ? "bg-white text-secondary rounded"
@@ -49,6 +50,7 @@ function FilterButton() {
                 <div
                   data-testid="language-button-option"
                   aria-hidden="true"
+                  onClick={(e) => handleChange("oldest", e)}
                   className={`${
                     active
                       ? "bg-white text-secondary rounded"
