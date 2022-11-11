08b61f4fbf8eeefe09c28a392c5998324e231150
shadybot97 <108574192+ShadyBot97@users.noreply.github.com>
2022-11-12 02:15:24 +0300

    Responsive Fixed

--- src/components/ProfileEdit/contact_skills_page.jsx
+++ src/components/ProfileEdit/contact_skills_page.jsx
@@ -1,12 +1,12 @@
 function formComponent({ props }) {
   return (
-    <div className="flex px-60 flex-col  w-80 items-start w-100 ">
+    <div className="flex small:pl-10 px-60 flex-col   w-80 items-start w-100 ">
       <h6 className="py-2">{props.name}</h6>
       {props.desc != null}
       <dt className="pb-4 whitespace-nowrap">{props.desc}</dt>
       <input
         placeholder={props.hint}
-        className="px-4  bg-gray-100 w-80 h-12 max-h-screen rounded-xl"
+        className="px-4 placeholder:text-xs  bg-gray-100 w-60 h-12 max-h-screen rounded-xl"
       />
     </div>
   );
@@ -14,11 +14,11 @@ function formComponent({ props }) {
 function SocialSection({ props }) {
   return (
     <div className="flex flex-col h-96 py-10 bg-gray-200 content-start items-start">
-      <h2 className="text-4xl py-12 px-60">{props.name}</h2>
+      <h2 className="text-4xl py-12 small:pl-10 px-60 ">{props.name}</h2>
       <div className=" justify-start flex  flex-wrap ">
-        {formComponent({ props: { hint: "Linkedin", name: "Linkedin" } })}
-        {formComponent({ props: { hint: "Github", name: "Github" } })}
-        {formComponent({ props: { hint: "Facebook", name: "Facebook" } })}
+        {formComponent({ props: { hint: 'Linkedin', name: 'Linkedin' } })}
+        {formComponent({ props: { hint: 'Github', name: 'Github' } })}
+        {formComponent({ props: { hint: 'Facebook', name: 'Facebook' } })}
       </div>
     </div>
   );
@@ -27,13 +27,13 @@ function SocialSection({ props }) {
 function ContactSection({ props }) {
   return (
     <div className="flex  flex-col h-96 bg-gray-200 content-start items-start ">
-      <h2 className="text-4xl py-12 px-60">{props.name}</h2>
+      <h2 className="text-4xl py-12 small:pl-10 px-60 ">{props.name}</h2>
       <div className=" justify-start flex flex-wrap">
-        {formComponent({ props: { hint: "E.g Duhok", name: "Location" } })}
+        {formComponent({ props: { hint: 'E.g Duhok', name: 'Location' } })}
         {formComponent({
-          props: { hint: "E.g shady97@gmail.com", name: "Email" },
+          props: { hint: 'E.g shady97@gmail.com', name: 'Email' },
         })}
-        {formComponent({ props: { hint: "+964 750 111 1111", name: "Phone" } })}
+        {formComponent({ props: { hint: '+964 750 111 1111', name: 'Phone' } })}
       </div>
     </div>
   );
@@ -41,13 +41,13 @@ function ContactSection({ props }) {
 function SkillsSection({ props }) {
   return (
     <div className="flex flex-col h-96 py-24 bg-gray-200 content-start items-start">
-      <h2 className="text-4xl py-12 px-60">{props.name}</h2>
+      <h2 className="text-4xl py-12 small:pl-10 px-60 ">{props.name}</h2>
       <div className=" justify-start flex flex-wrap  ">
         {formComponent({
           props: {
-            hint: "Skills",
-            name: "Skills",
-            desc: "Add skills as commas perated values.",
+            hint: 'Skills',
+            name: 'Skills',
+            desc: 'Add skills as commas perated values.',
           },
         })}
       </div>
@@ -58,9 +58,9 @@ function SkillsSection({ props }) {
 function ContactSkills() {
   return (
     <div>
-      {ContactSection({ props: { name: "Contact" } })}
-      {SocialSection({ props: { name: "Social" } })}
-      {SkillsSection({ props: { name: "Skills" } })}
+      {ContactSection({ props: { name: 'Contact' } })}
+      {SocialSection({ props: { name: 'Social' } })}
+      {SkillsSection({ props: { name: 'Skills' } })}
     </div>
   );
 }
