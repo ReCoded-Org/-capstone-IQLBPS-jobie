4b3b0b43f2f405b68e1c167f6af49c227f451efc
allan <allansaleh95@gmail.com>
2022-11-12 11:33:16 +0300

    changed data

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -31,7 +31,7 @@ export default function Contact() {
               <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
               <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
             </svg>
-            <span className="pl-3">allan@re-coded.com</span>
+            <span className="pl-3">allan@gmail.com</span>
           </div>
           <div className=" flex">
             <svg
@@ -46,7 +46,7 @@ export default function Contact() {
                 clipRule="evenodd"
               />
             </svg>
-            <span className="pl-3">+964-750-797-3373</span>
+            <span className="pl-3">+964-750-444-4444</span>
           </div>
         </div>
       </div>
