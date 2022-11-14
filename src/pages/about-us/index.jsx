5cafd1047cb3c5c49237a2d6ceb206babd039541
ayaali333 <1aya.dn.ali@gmail.com>
2022-11-14 20:48:05 +0300

    about-us component

---
+++ src/pages/about-us/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import AboutHero from "../../components/aboutHero/AboutHero";
+import HowItWorks from "../../components/About/HowItWorks/HowItWorks";
+import MeetOurTeam from "../../components/MeetOurTeam/MeetOurTeam";
+import OurCollaborators from "../../components/OurCollaborators/OurCollaborators";
+import WhyChooseUs from "../../components/whyChooseUs/WhyChooseUs";
+import {teamMembers} from "../../data/teamData"
+import { showCaseData} from "../../data/showCaseData"
+
+function AboutPage() {
+  return (
+    <div>
+      <AboutHero title="find your job" sub="live your dream" img="/assets/aboutHero/one.svg" />
+      <HowItWorks />
+      <MeetOurTeam teamMembers={teamMembers} />
+      <OurCollaborators  showCaseData={showCaseData}/>
+      <AboutHero />
+      <HowItWorks />
+      <WhyChooseUs />
+      
+    </div>
+  );
+}
+
+export default AboutPage;
