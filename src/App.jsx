333fe2dbbf0a2172ddae0bd78cae72d1e3854479
sunny <107579067+lorem42@users.noreply.github.com>
2022-11-17 22:17:23 +0300

    Contact Us Hero done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
-import Hero from './components/Hero/Hero';
-import WyChooseUs from './components/whyChooseUs/WhyChooseUs';
+// import Hero from './components/Hero/Hero';
+// import WyChooseUs from './components/whyChooseUs/WhyChooseUs';
 import './App.css';
-import LatestJobs from './components/LatestJob/LatestJobs';
+// import LatestJobs from './components/LatestJob/LatestJobs';
+import ContactUsHero from "./components/Hero/ContactUsHero/ContactUsHero";
 
-const data = [
-  { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
-  { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
-  { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
-  { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
-  { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
-];
-const header = ['Position', 'Salary', 'Date', ''];
+// const data = [
+//   { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
+//   { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
+//   { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
+//   { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
+//   { position: 'Web Developer', salary: '2000$-3000$', date: '10/10/2022' },
+// ];
+// const header = ['Position', 'Salary', 'Date', ''];
 function App() {
   return (
     <div>
-      <Hero />
-      <LatestJobs data={data} headers={header} />
-      <WyChooseUs />
+      {/* <Hero /> */}
+      <ContactUsHero/>
+      {/* <LatestJobs data={data} headers={header} />
+      <WyChooseUs /> */}
     </div>
+
   );
 }
 
---
+++ src/components/Hero/ContactUsHero/ContactUsHero.jsx
@@ -0,0 +1,7 @@
+const ContactUsHero = () => {
+      return (
+        <img className="max-w-full h-auto mx-auto" src="/assets/contactUsHero.jpg" alt="Contact us page hero"/>
+      )
+}
+
+export default ContactUsHero
