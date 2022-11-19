1e9206d798291081b4758aa9e946f5553cb02c80
ayaali333 <1aya.dn.ali@gmail.com>
2022-11-19 18:44:50 +0300

    RoadMaps Navbar component

---
+++ src/pages/roadmaps/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import RoadMaps from "../../components/Roadmaps/RoadMaps";
+
+const Roadmaps = () => {
+  return (
+    <div>
+      <RoadMaps />
+    </div>
+  );
+};
+export default Roadmaps;
