6aaf0c5aa78fa045ae94618439a2719405f01093
allan <allansaleh95@gmail.com>
2022-11-21 18:37:36 +0300

    ran yarn fix

--- src/components/OurCustomers/__tests__/our_customers.test.jsx
+++ src/components/OurCustomers/__tests__/our_customers.test.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import OurCustomers from '../our_customers';
+import React from "react";
+import renderer from "react-test-renderer";
+import OurCustomers from "../our_customers";
 
-
-it('renders correctly when there are no items', () => {
+it("renders correctly when there are no items", () => {
   const tree = renderer.create(<OurCustomers />).toJSON();
   expect(tree).toMatchSnapshot();
 });
