0caef4d40545226878b3b83808737f98d49653ca
ayaali333 <1aya.dn.ali@gmail.com>
2022-11-22 01:17:11 +0300

    Add  aboutHero Component

--- src/pages/roadmaps/index.js
+++ src/pages/roadmaps/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import RoadMaps from "../../components/Roadmaps/RoadMaps";
+import AboutHero from "../../components/aboutHero/AboutHero";
 
 const Roadmaps = () => {
   return (
     <div>
       <RoadMaps />
+      <AboutHero title="Developer Roadmap" sub="Follow these roadmaps and become a hero in any path you chooese!" img="/assets/aboutHero/one.jpg" />
     </div>
   );
 };
