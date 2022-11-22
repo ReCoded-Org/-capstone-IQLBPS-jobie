d5bc25b15dab446875c43852ffc3fb57b49cf2f6
Iman <96542573+Imanzuher@users.noreply.github.com>
2022-11-22 04:46:23 +0300

    signup authentication done (#128)
    * signup authentication done
    
    * updated snapshot testing to pass
    
    Co-authored-by: allan <allansaleh95@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { React, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+import { signup, selectUser, signout } from "./features/user/userSlice";
+import { auth, onAuthStateChanged } from "./firebase";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import SignUp from "./components/signup/SignUp";
@@ -9,13 +12,34 @@ import ProfilePage from "./pages/profile";
 import Index from "./pages/index";
 import Footer from "./components/Footer/Footer";
 import CombiningFilterComponents from "./components/CombiningComponents/CombiningFilterComponents";
+import SignOut from "./components/signup/SignOut";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        // user is logged in
+        dispatch(
+          signup({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            fullname: userAuth.fullname,
+            password: userAuth.password,
+          })
+        );
+      } else {
+        dispatch(signout());
+      }
+    });
+  }, []);
   return (
     <div>
       <Navbar />
+      <SignOut />
       <Routes>
-        <Route path="/" element={<Index />} />
+        <Route path="/" element={user ? <Index /> : <SignUp />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/about-us" element={<AboutPage />} />
