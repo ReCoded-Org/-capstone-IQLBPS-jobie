458ca8b7960a0e71dfd5f699aaaf427d86deb7da
allan <allansaleh95@gmail.com>
2022-11-27 05:50:36 +0300

    updated snapshot tests

--- src/components/Login/__test__/Login.test.js
+++ src/components/Login/__test__/Login.test.js
@@ -1,7 +1,9 @@
 import renderer from "react-test-renderer";
-
 import Login from "../Login";
 
+jest.mock("react-router-dom", () => ({
+  useNavigate: () => ({ t: (key) => key }),
+}));
 it("renders correctly when the component matches the snapshot", () => {
   const tree = renderer.create(<Login />).toJSON();
   expect(tree).toMatchSnapshot();
